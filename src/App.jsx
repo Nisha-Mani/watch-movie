34ffeb7f60285c0e8b5bc32fbd6de0bff11eef22
Nisha-Mani <nishamani195@gmail.com>
2025-04-19 01:13:53 +0530

    initialising proj base

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const App = () => {
+  return (
+    <div>
+      <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+
+    </div>
+  )
+}
+
+export default App
+
